9a9d7fb2573155bdaf1b79a5d9fb62335a8da979
Nithin N <57832822+Nithin1506200@users.noreply.github.com>
2021-08-11 12:56:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -129,3 +129,32 @@ engbtn.addEventListener('click',()=>{
         map:map
     });
 });
+
+
+const slider=document.querySelector(".container1");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+ 
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+ 
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+ 
+});
